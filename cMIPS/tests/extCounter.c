77cdbdb5a3364495d84edb30d9d3adfc9eba6382
Roberto Hexsel <roberto@inf.ufpr.br>
2017-05-10 11:51:40 -0300

    improved test for external counter

--- cMIPS/tests/extCounter.c
+++ cMIPS/tests/extCounter.c
@@ -7,7 +7,11 @@
 #define FALSE (0==1)
 #define TRUE  !(FALSE)
 
-#define N 4
+// convert small integer (i<16) to hexadecimal digit
+#define i2c(a) ( ((a) < 10)   ? ((a)+'0') : (((a)+'a')-10) )
+
+
+#define N 4                     // must be less than 16
 #define CNT_VALUE 0x40000040    // set count to 64 cycles
 
 void main(void) {
@@ -16,7 +20,8 @@ void main(void) {
   newValue = CNT_VALUE;
 
   for (i=0; i < N; i++) {       // repeat N rounds
-    print(i);                   // print number of round
+    to_stdout( i2c(i) );        // print number of round
+    to_stdout('\n');
 
     newValue = CNT_VALUE + (i<<3);
     startCounter(newValue, 0);  // num cycles increases with i, no interrupts
