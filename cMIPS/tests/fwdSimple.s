b8dffc4e425ef2154d1c7132d12c03322f7493a2
Roberto Hexsel <roberto@inf.ufpr.br>
2018-04-02 11:29:52 -0300

    improved simple test for ALU forwarding

--- cMIPS/tests/fwdSimple.s
+++ cMIPS/tests/fwdSimple.s
@@ -13,26 +13,27 @@ _start: la   $15, x_IO_BASE_ADDR  # stdout
 	nop
 	nop
 
-        add $5,$5,$1    # $5 + 1
+        add $6, $5, $1  # $5 + 1
 	nop		# remove on first test
 	nop		# remove on second test
 	nop		# remove on third test
-        sw  $5, 0($15) 	# print $5
 
-        add $5,$5,$1    # $5 + 1
+        add $7, $6, $1  # $6 + 1
 	nop		# remove on first test
 	nop		# remove on second test
 	nop		# remove on third test
-        sw  $5, 0($15) 	# print $5
 	
-        add $5,$5,$1    # $5 + 1
+        add $8, $7, $1  # $7 + 1
 	nop		# remove on first test
 	nop		# remove on second test
 	nop		# remove on third test
-        sw  $5, 0($15) 	# print $5
 
+        sw  $6, 0($15) 	# print $6
+        sw  $7, 0($15) 	# print $7
+        sw  $8, 0($15) 	# print $8
+	
 	nop	# do not touch these 5 NOPS
-	nop	# flush the pipeline
+	nop	# they are here to flush the pipeline
 	nop
 	nop
 	nop
